11dc26a7d20a09003047dcf8f39ceb84e4e34442
Stefi <smanastirska@gmail.com>
2024-09-02 11:30:42 +0200

    backend added

---
+++ Backend/src/test/java/com/stefi/YouSong/YouSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefi.YouSong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
